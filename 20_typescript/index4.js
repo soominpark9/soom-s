767e5bedb28dbea75f187320a5e1444983f1d0e7
park <goslwjddl@naver.com>
2023-01-10 18:07:48 +0900

    study:typescript class 타입지정

--- 20_typescript/index4.js
+++ 20_typescript/index4.js
@@ -16,7 +16,7 @@ var Person1 = /** @class */ (function () {
     };
     return Person1;
 }());
-//   new Person1(); //person1안에 있던 속성을 물려받은 object가 남음
+//new Person1(); //person1안에 있던 속성을 물려받은 object가 남음
 var 사람1 = new Person1("kim"); //변수에다가 저장해서 써도됨 //파라미터자리에 kim넣으면 kim나옴
 var 사람2 = new Person1("park"); //park 출력
 사람1.함수("안녕"); //안녕을 a라는 파라미터에 넣어 함수를 실행해주세요 ->안녕안녕
@@ -31,3 +31,25 @@ var Person2 = /** @class */ (function () {
 new Person2(); //person2안에 있던 속성을 물려받은 object가 남음
 var 사람3 = new Person2();
 console.log(사람3.data);
+//실습1  Car 클래스를 만들고 싶습니다.
+/*1. 대충 { model : '소나타', price : 3000 } 이렇게 생긴 object를 복사해주는 class를 만들어보십시오.
+2. 그리고 복사된 object 자료들은 .tax() 라는 함수를 사용가능한데 현재 object에 저장된 price의 10분의1을 출력해주어야합니다.
+3. model과 price 속성의 타입지정도 알아서 잘 해보십시오. tax() 함수의 return 타입도요.*/
+var Car = /** @class */ (function () {
+    function Car(a, b) {
+        this.model = a;
+        this.price = b;
+    }
+    Car.prototype.tax = function () {
+        return this.price * 0.1;
+    };
+    return Car;
+}());
+var car1 = new Car("소나타", 3000);
+console.log(car1); //콘솔창 출력결과는 { model : '소나타', price : 3000 }
+console.log(car1.tax()); //콘솔창 출력결과는 300
+//실습2 class인데 파라미터가 잔뜩 들어가는 class Word를 만들어봅시다.
+/* 1. object 만들 때 new Word() 소괄호 안에 숫자 혹은 문자를 입력하면
+2. 숫자는 전부 object 안의  num 속성 안에 array 형태로 저장되고
+3. 문자는 전부 object 안의 str 속성 안에 array 형태로 저장되는 class를 만들어봅시다.
+4. class 만들 때 넣을 수 있는 숫자와 문자 갯수는 일단 제한은 없습니다. 그리고 타입 빼먹지 말기 */
